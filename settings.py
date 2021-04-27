d6dce28414318fa7a90089c4f575adc30f42a97d
zakirovrj <rjzakirov@gmail.com>
2021-04-27 20:58:49 +0200

    HW_4 - partially added validation

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
+from string import punctuation
+
 DB_NAME = 'scheduler.db'
 VALID_STATUSES = ['New', 'In Progress', 'Closed']
+SYMBOLS = set(punctuation)
+
+print(SYMBOLS)  # {'{', '"', ';', '}', '$', '%', '-', '/', "'", '=', '>', '[', '~', '`', '<', '.', '@', '?', ':', '(', '#', '^', ')', '+', '*', '!', '|', '&', ']', ',', '_', '\\'}
